fbec2f65d02b17c6c0112484375a538dc3449ca7
Ben Pski <ben@pepski.com>
2018-10-17 20:40:24 -0600

    Improve the interface a bit. Have basic symbolic computations.

--- src/CGA.hs
+++ src/CGA.hs
@@ -6,40 +6,26 @@ contains the definition for conformal geometric algebra
 
 import GA
 
-data CGA = E1 | E2 | E3 | Ep | Em deriving (Eq, Ord, Show)
-
-instance InnerProduct CGA where
-    dot E1 E1 = 1
-    dot E2 E2 = 1
-    dot E3 E3 = 1
-    dot Ep Ep = 1
-    dot Em Em = -1
-    dot _ _ = 0
-
-zero, one :: MVector CGA
-zero = toMulti []
-one = toMulti [(Blade [], 1)]
+e1, e2, e3, em, ep, n0, nInf :: Multivector Sym Mother
+e1 = toMulti [(Blade [Pos "e1" 1], (Lit 1))]
+e2 = toMulti [(Blade [Pos "e2" 2], (Lit 1))]
+e3 = toMulti [(Blade [Pos "e3" 3], (Lit 1))]
+ep = toMulti [(Blade [Pos "e+" 4], (Lit 1))]
+em = toMulti [(Blade [Neg "e-" 5], (Lit 1))]
 
 scalar s = toMulti [(Blade [], s)]
 vector e = toMulti [(Blade [e], 1)]
 
-e1 :: MVector CGA
-e1 = toMulti [(Blade [E1], 1)]
-e2 = toMulti [(Blade [E2], 1)]
-e3 = toMulti [(Blade [E3], 1)]
-ep = toMulti [(Blade [Ep], 1)]
-em = toMulti [(Blade [Em], 1)]
-
-n0 = scale (1/2) (add ep em)
-nInf = sub em ep
+n0 = scale (Lit $ 1/2) (mAdd ep em)
+nInf = mSub em ep
 
 --project a R3 vector to CGA
-proj a = (n0 `add` a `add` (scale (1/2) (a `mult` a `mult` nInf)))
+proj a = (n0 `mAdd` a `mAdd` (scale (Lit $ 1/2) (a `mMul` a `mMul` nInf)))
 
 --retract a = a `sub` n0 `sub` (scalar (1/2) `mult`)
 
 --translate
-trans a b = ((scalar 1) `sub` ((scalar (1/2)) `mult` a ` mult` nInf)) `mult` b `mult` ((scalar 1) `add` ((scalar (1/2)) `mult` a ` mult` nInf))
+--trans a b = ((scalar 1) `sub` ((scalar (1/2)) `mult` a ` mult` nInf)) `mult` b `mult` ((scalar 1) `add` ((scalar (1/2)) `mult` a ` mult` nInf))
 
 --rotation
 --rot theta plane m =
--- src/GA.hs
+++ src/GA.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FunctionalDependencies #-}
 module GA where
 
 import qualified Data.Map.Strict as M
@@ -5,65 +6,26 @@ import Data.List (intercalate)
 
 {-
 
-This time around want to avoid having to carry around the inner product space definition
-To do this just have to require an inner product space be defined on the base vectors
+Want a nicer representation for multivectors
+this way canonicalizing the form doesn't require doing psedo raising of the blades to multivectors
 
-So the layout goes something like:
-    define the generating vectors for the space
-    define the inner product space for the generating vectors
-    the arithmetic simply follows
-
-also making it a bit clearer that a multivector is a sum of blades with an associated coefficient
-
-issue is now that defining the vector space isn't quite as simple
-    no more saying it is orthonormal R^n, at least for now
-
-I think the canonical form stuff doesn't necessarily work for null vectors
-but it is fine for the orthonormal ones
+Notably everything is much simpler when restriciting the generating vectors to being orthonomal (a subalgebra of R(inf,inf), the mother algebra)
+This means that arbitrary bases do not work (e.g., directly defined by null vectors), but they can be operated on like they are by first defining the orthonomral basis and then defining and using the null vectors
 
 -}
 
---inner product space
-class InnerProduct a where
-    dot :: a -> a -> Double
+--defined on a scalar and the generating vectors
+newtype Multivector s a = Multivector (M.Map (Blade a) s) deriving (Eq, Ord)
 
---the canonical form of a term
---sorting and trasforming via the inner product
-canon :: (InnerProduct a, Ord a) => (Blade a, Double) -> [(Blade a, Double)]
-canon a@(Blade vec, coe) = case vec of
-                            [] -> [a] --no way it could be out of order
-                            [x] -> [a]
-                            [x,y] -> blades $ removeZeros $ scale coe (multV x y) --simply do the multiplication
-                            --have to step through it
-                            _ ->  removeZeros $ scale coe $ helper [] vec
-    where
-        multV a b
-            | a == b = [([], dot a b)]
-            | a < b = [([a,b], 1)]
-            | a > b = [([],2*(dot b a)), ([b,a], -1)]
-        removeZeros = filter (\(_,c) -> c /= 0)
-        scale coe = fmap (\(v,c) -> (v, c*coe))
-        blades = fmap (\(v,c) -> (Blade v,c))
+instance (Show a, Show s, Ring s, Eq s) => Show (Multivector s a) where
+    show (Multivector m)
+        | M.size m == 0 = "0"
+    show (Multivector m) = intercalate " + " $ fmap showTerm $ M.assocs m
+        where
+            showTerm (Blade vec, s)
+                | s == one = if (length vec == 0) then "1" else show (Blade vec)
+                | otherwise = (show s) ++ (show (Blade vec))
 
-        helper front [] = [(Blade front, 1)] --completed
-        helper front [x] = [(Blade $ front ++ [x], 1)]
-        helper front (a:b:xs)
-            | a < b = helper (front ++ [a]) (b:xs) --already in order
-            | otherwise = let res = multV a b --get the actual form
-                              terms = fmap (\(v,c) -> (Blade $ front ++ v ++ xs, c)) res
-                          in concatMap canon terms
-
-
---something like this
---given a coefficient and the underlying blade, get it into canonical form
---may have to create multiple blades though, like in a null space
-    --a nullspace can have ab = 2*(a.b) - ba
-    --as the canonical form of the term
---in an orthonormal system that should never happen
-
-
---a blade is the free monoid on the generating vectors, but it has to be resricted by the inner product
---so it is isomorphic to a list in its definition, but in applications duplicate items have to be removed in a formulaic way
 newtype Blade a = Blade { vecs :: [a] } deriving (Eq, Ord)
 
 instance Show a => Show (Blade a) where
@@ -72,56 +34,179 @@ instance Show a => Show (Blade a) where
 instance Functor Blade where
     fmap f (Blade xs) = Blade (fmap f xs)
 
---a multivector
-newtype MVector a = MVector (M.Map (Blade a) Double) deriving (Eq, Ord)
+--mostly for testing
+--the "mother algebra"
+--pick out some of the vecotrs from it to work with, have to be careful about the selection
+data Mother = Pos String Int
+            | Neg String Int
+            deriving (Eq)
 
-instance Show a => Show (MVector a) where
-    show (MVector m)
-        | M.size m == 0 = "0"
-    show (MVector m) = intercalate " + " $ fmap showTerm $ M.assocs m
-        where
-            showTerm (blade, 1)
-                | (length $ vecs blade) == 0 = "1"
-                | otherwise = (show blade)
-            showTerm (blade, s) = (show s) ++ (show blade)
+instance Ord Mother where
+    (Pos _ i) <= (Pos _ j) = i <= j
+    (Pos _ _) <= (Neg _ _) = True
+    (Neg _ _) <= (Pos _ _) = False
+    (Neg _ i) <= (Neg _ j) = i <= j
 
-isBlade :: MVector a -> Bool
-isBlade (MVector m) = (1==) $ M.size m
+instance Show Mother where
+    show (Pos a _) = a
+    show (Neg a _) = a
 
-isGrade :: Int -> MVector a -> Bool
-isGrade n (MVector m) = let lens = n : fmap (\(Blade vec,_) -> length vec) (M.assocs m) in and $ zipWith (==) lens (tail lens)
+--sorta dorky that they are the same definition
+instance InnerProduct Mother Double where
+    dot (Pos _ _) (Neg _ _) = zero
+    dot (Neg _ _) (Pos _ _) = zero
+    dot (Neg _ i) (Neg _ j)
+        | i == j = (neg one)
+        | otherwise = zero
+    dot (Pos _ i) (Pos _ j)
+        | i == j = one
+        | otherwise = zero
 
-extractGrade :: Int -> MVector a -> MVector a
-extractGrade n (MVector m) = MVector $ M.filterWithKey (\(Blade vec) _ -> length vec == n) m
+instance InnerProduct Mother Sym where
+    dot (Pos _ _) (Neg _ _) = zero
+    dot (Neg _ _) (Pos _ _) = zero
+    dot (Neg _ i) (Neg _ j)
+        | i == j = (neg one)
+        | otherwise = zero
+    dot (Pos _ i) (Pos _ j)
+        | i == j = one
+        | otherwise = zero
 
-unMulti :: MVector a -> [(Blade a, Double)]
-unMulti (MVector a) = M.assocs a
-toMulti :: (Ord a, InnerProduct a) => [(Blade a, Double)] -> MVector a
-toMulti = MVector . M.filter (/=0) . M.fromListWith (+)
 
-add :: (Ord a, InnerProduct a) => MVector a -> MVector a -> MVector a
-add (MVector a) (MVector b) = toMulti $ (concatMap canon $ M.assocs a) ++ (concatMap canon $ M.assocs b)
+--inner product space
+class InnerProduct a s | s -> a where
+    dot :: a -> a -> s
 
-sub :: (Ord a, InnerProduct a) => MVector a -> MVector a -> MVector a
-sub a b = add a (scale (-1) b)
+--just want an abstraction for 0 and 1 numerically, I believe this is a Ring
+--i guess the way it will be used means it can be any scalar field for the coefficients
+class Ring a where
+    zero :: a
+    one :: a
+    add :: a -> a -> a
+    sub :: a -> a -> a
+    sub a b = add a (neg b)
+    neg :: a -> a
+    neg a = sub zero a
+    mul :: a -> a -> a
+    {-# MINIMAL zero, one, add, mul, (sub | neg)#-}
 
-mult :: (Ord a, InnerProduct a) => MVector a -> MVector a -> MVector a
-mult (MVector a) (MVector b) = toMulti $ concat $ mult <$> M.assocs a <*> M.assocs b
+instance Ring Double where
+    zero = 0
+    one = 1
+    add a b = a + b
+    mul a b = a * b
+    neg a = negate a
+
+data Sym = Var String
+         -- | Zero
+         -- | One
+         | Lit Double
+         | Add Sym Sym
+         | Mul Sym Sym
+         | Negative Sym
+         deriving (Eq)
+
+instance Show Sym where
+    show (Var s) = s
+    show (Lit d) = show d
+    --show Zero = "0"
+    --show One = "1"
+    show (Add a b) = "(" ++ show a ++ " + " ++ show b ++ ")"
+    show (Mul a b) = "(" ++ show a ++ " * " ++ show b ++ ")"
+    show (Negative a) = "(" ++ "-" ++ show a ++ ")"
+
+instance Ring Sym where
+    zero = Lit 0
+    one = Lit 1
+
+    add (Lit 0) b = b
+    add a (Lit 0) = a
+    add (Lit a) (Lit b) = Lit $ a+b
+    add a b = Add a b
+
+    mul (Lit 0) b = (Lit 0)
+    mul a (Lit 0) = (Lit 0)
+    mul (Lit 1) b = b
+    mul a (Lit 1) = a
+    mul (Lit a) (Lit b) = Lit $ a*b
+    mul (Negative a) (Negative b) = mul a b
+    mul (Negative a) b = Negative (mul a b)
+    mul a (Negative b) = Negative (mul a b)
+    mul a b = Mul a b
+
+    neg (Lit 0) = (Lit 0)
+    neg (Negative a) = a
+    neg (Lit a) = (Lit (-a))
+    neg a = (Negative a)
+
+
+makeMulti ::(Eq s, Ring s, InnerProduct a s, Ord a) => [(Blade a,s)] -> Multivector s a
+makeMulti = Multivector . M.filter (/=zero) . M.fromListWith add
+
+canon :: (Eq s, Ring s, InnerProduct a s, Ord a) => Multivector s a -> Multivector s a
+canon (Multivector m) = makeMulti $ fmap canonBlade (M.assocs m)
     where
-        mult (Blade vecs1, coe1) (Blade vecs2, coe2) = canon $ (Blade (vecs1 ++ vecs2), coe1*coe2 )
+        canonBlade (Blade vec, c) = case vec of
+                                        [] -> (Blade vec, c)
+                                        [x] -> (Blade vec, c)
+                                        [x,y] -> case compare x y of
+                                                    EQ -> (Blade [], mul c (dot x y))
+                                                    LT -> (Blade vec, c)
+                                                    GT -> (Blade [y,x], neg c)
+                                        --due to having the orthonormal assumption this can only ever return a single blade
+                                        _ -> let (Blade vec', c') = helper [] vec in (Blade vec', mul c' c)
+        -- should do this more in a back to front manner
+        helper front [] = (Blade front, one)
+        helper front [x] = (Blade $ front ++ [x], one)
+        helper front (a:b:xs)
+            | a < b = helper (front ++ [a]) (b:xs)
+            | otherwise = let (Blade vec', c') = canonBlade (Blade [a,b], one)
+                          in (\(a, c) -> (a, mul c c')) $ helper [] (front ++ vec' ++ xs) --have to rescale at the end
 
-outer :: (InnerProduct a, Ord a) => MVector a -> MVector a -> MVector a
-outer (MVector a) (MVector b) = toMulti $ concat $ outerBlade <$> (M.assocs a) <*> (M.assocs b)
+isBlade :: Multivector s a -> Bool
+isBlade (Multivector m) = (1==) $ M.size m
+
+isGrade :: Int -> Multivector s a -> Bool
+isGrade n (Multivector m) = let lens = n : fmap (\(Blade vec, _) -> length vec) (M.assocs m)
+                            in and (zipWith (==) lens (tail lens))
+
+extractGrade :: Int -> Multivector s a -> Multivector s a
+extractGrade n (Multivector m) = Multivector $ M.filterWithKey (\(Blade vec) _ -> length vec == n) m
+
+unMulti :: Multivector s a -> [(Blade a, s)]
+unMulti (Multivector a) = M.assocs a
+toMulti :: (Ord a, InnerProduct a s, Ring s, Eq s) => [(Blade a,s)] -> Multivector s a
+toMulti = Multivector . M.filter (/=zero) . M.fromListWith add
+
+--are the operations a ring?
+mAdd :: (Ring s, InnerProduct a s, Ord a, Eq s) => Multivector s a -> Multivector s a -> Multivector s a
+mAdd (Multivector a) (Multivector b) = canon $ toMulti $ (M.assocs a) ++ (M.assocs b)
+
+mSub a b = mAdd a (scale (neg one) b)
+
+mMul :: (Ring s, InnerProduct a s, Ord a, Eq s) => Multivector s a -> Multivector s a -> Multivector s a
+mMul (Multivector a) (Multivector b) = canon $ toMulti $ mult <$> (M.assocs a) <*> (M.assocs b)
     where
-        outerBlade x@(Blade vec1, coe1) y@(Blade vec2, coe2) = unMulti $ extractGrade ((length vec1)+(length vec2)) (mult (toMulti [x]) (toMulti [y]))
+        mult (Blade vec1, c1) (Blade vec2, c2) = (Blade $ vec1 ++ vec2, mul c1 c2)
 
-inner :: (InnerProduct a, Ord a) => MVector a -> MVector a -> MVector a
-inner (MVector a) (MVector b) = toMulti $ concat $ innerBlade <$> (M.assocs a) <*> (M.assocs b)
+outer :: (Ring s, InnerProduct a s, Ord a, Eq s) => Multivector s a -> Multivector s a -> Multivector s a
+outer (Multivector a) (Multivector b) = canon $ toMulti $ concat $ outerBlade <$> (M.assocs a) <*> (M.assocs b)
     where
-        innerBlade x@(Blade vec1, coe1) y@(Blade vec2, coe2) = unMulti $ extractGrade ((length vec1)-(length vec2)) (mult (toMulti [x]) (toMulti [y]))
+        outerBlade x@(Blade vec1, c1) y@(Blade vec2, c2) = unMulti $ extractGrade ((length vec1) + (length vec2)) (mMul (toMulti [x]) (toMulti [y]))
 
-scale s (MVector a) = MVector $ M.map (*s) a
-
-leftContract (MVector a) (MVector b) = toMulti $ concat $ innerBlade <$> (M.assocs a) <*> (M.assocs b)
+inner :: (Ring s, InnerProduct a s, Ord a, Eq s) => Multivector s a -> Multivector s a -> Multivector s a
+inner (Multivector a) (Multivector b) = canon $ toMulti $ concat $ innerBlade <$> (M.assocs a) <*> (M.assocs b)
     where
-        innerBlade x@(Blade vec1, coe1) y@(Blade vec2, coe2) = unMulti $ extractGrade ((length vec1)-(length vec2)) (mult (toMulti [x]) (toMulti [y]))
+        innerBlade x@(Blade vec1, c1) y@(Blade vec2, c2) = unMulti $ extractGrade (abs $ (length vec1) - (length vec2)) (mMul (toMulti [x]) (toMulti [y]))
+
+leftContract :: (Ring s, InnerProduct a s, Ord a, Eq s) => Multivector s a -> Multivector s a -> Multivector s a
+leftContract (Multivector a) (Multivector b) = canon $ toMulti $ concat $ lBlade <$> (M.assocs a) <*> (M.assocs b)
+    where
+        lBlade x@(Blade vec1, c1) y@(Blade vec2, c2) = unMulti $ extractGrade ((length vec2) - (length vec1)) (mMul (toMulti [x]) (toMulti [y]))
+
+rightContract :: (Ring s, InnerProduct a s, Ord a, Eq s) => Multivector s a -> Multivector s a -> Multivector s a
+rightContract (Multivector a) (Multivector b) = canon $ toMulti $ concat $ rBlade <$> (M.assocs a) <*> (M.assocs b)
+    where
+        rBlade x@(Blade vec1, c1) y@(Blade vec2, c2) = unMulti $ extractGrade ((length vec1) - (length vec2)) (mMul (toMulti [x]) (toMulti [y]))
+
+scale s (Multivector m) = canon $ Multivector $ M.map (mul s) m
